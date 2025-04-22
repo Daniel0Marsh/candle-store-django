d2ec6085cebf430fca6c45de303c60e3f40cca8a
Daniel0Marsh <danielmarsh1000@protonmail.com>
2025-04-22 19:53:26 +0100

    Add product search functionality and UI improvements
    Introduced a new ProductSearchView and related templates to enable product search functionality. Updated URLs, views, and base HTML for search integration. Improved product display layouts with better alignment and responsiveness across templates.

--- source/home/urls.py
+++ source/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import HomePageView, AboutPageView, PrivacyPolicyPageView, TermsOfServicePageView
+from . views import HomePageView, ProductSearchView, AboutPageView, PrivacyPolicyPageView, TermsOfServicePageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('search/', ProductSearchView.as_view(), name='product_search'),
     path('about-us/', AboutPageView.as_view(), name='about'),
     path('privacy-policy/', PrivacyPolicyPageView.as_view(), name='privacy_policy'),
     path('terms-of-service/', TermsOfServicePageView.as_view(), name='terms_of_service'),
